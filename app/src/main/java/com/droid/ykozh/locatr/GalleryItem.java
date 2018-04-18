6070bccb99a91cdbc3700aa4b65b3ab9c3bd1cc6
Yaroslav Kozhumyaka <y.kozhumyaka@gmail.com>
2018-04-18 11:55:24 +0300

    Initial commit

---
+++ app/src/main/java/com/droid/ykozh/locatr/GalleryItem.java
@@ -0,0 +1,51 @@
+package com.droid.ykozh.locatr;
+
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+    private double mLat;
+    private double mLon;
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+
+    public double getLat() {
+        return mLat;
+    }
+    public void setLat(double lat) {
+        mLat = lat;
+    }
+    public double getLon() {
+        return mLon;
+    }
+    public void setLon(double lon) {
+        mLon = lon;
+    }
+}
